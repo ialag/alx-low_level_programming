f6894f44a20e959a04e365509eaade21664a0610
ialag <ialaghita94@gmail.com>
2023-04-20 16:29:22 -0700

    ghs

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,9 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
  * main - A c program that prints a line
  * Return 1 (Success)
 */
 int main(void)
-{fput("and that piece of art is useful\" - Dora Kor par, 2015-10-19\r", stdout);
+{write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
