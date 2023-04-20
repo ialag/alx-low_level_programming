bd490e3171ae1041df8b89971e4fb7012beba964
ialag <ialaghita94@gmail.com>
2023-04-20 16:53:04 -0700

    slks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a line
- * Return 1 (Success)
+*main - A c program that prints a line
+*Return: 1 (Success)
 */
 int main(void)
 {write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
