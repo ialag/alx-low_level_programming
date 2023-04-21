2134a5f76004a13d29117fa71f57afc284bbc56c
ialag <ialaghita94@gmail.com>
2023-04-21 11:33:09 -0700

    gh

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - print single digit numbers
+ * Return: Always (Success)
+*/
+int main(void)
+{int d;
+for (d = '0'; d <= '9'; d++)
+putchar(d);
+putchar('\n');
+return (0);
+}
