28d7b6bfa9710bfdce8bfc2d0ac1b2745783ec50
ialag <ialaghita94@gmail.com>
2023-04-21 11:55:59 -0700

    dfg

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints possible combinations of single digit numbers
+ * Return: Always (Success)
+*/
+int main(void)
+{int d;
+for (d = '0'; d <= '9'; d++)
+{
+putchar(d);
+if (d != '9')
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
