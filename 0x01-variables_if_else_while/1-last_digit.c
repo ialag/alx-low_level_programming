438a4b723ec32368a974fc337dda37f01751d87b
ialag <ialaghita94@gmail.com>
2023-04-21 10:49:56 -0700

    hh

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ if (lastd > 5)
 else if (lastd == 0)
 {printf("Last digit of %d is %d and is 0\n", n, lastd);
 }
-else if (lastd < 6 && lastd !=0)
+else if (lastd < 6 && lastd != 0)
 {printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastd);
 }
 return (0);
