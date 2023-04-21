d69b12bbd2b183fb2630bf4c29ea6f70847784ef
ialag <ialaghita94@gmail.com>
2023-04-21 11:38:10 -0700

    sd

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints lowercase alphabet in reverse
+ * Return: Always (Success)
+*/
+int main(void)
+{ char c;
+for (c = 'z'; c <= 'a'; c++)
+putchar(c);
+putchar('\n');
+return (0);
+}
