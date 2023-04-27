81f44422fa5f48d56077bf559176f19effdd3f7d
ialag <ialaghita94@gmail.com>
2023-04-27 14:28:24 -0700

    sdd

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -5,10 +5,10 @@
  * Return: empty
 */
 void print_triangle(int size)
-{
+{       
 int x, y, z;
 if (size <= 0)
-{
+{       
 _putchar('\n');
 }
 else
@@ -16,7 +16,7 @@ else
 for (x = 0; x < size; x++)
 {
 for (y = size - x; y > 1; y--)
-{ 
+{
 _putchar(32);
 }
 for (z = 0; z <= x; z++)
@@ -24,6 +24,6 @@ for (z = 0; z <= x; z++)
 _putchar(35);
 }
 _putchar('\n')';
-}
+}               
 }
 }
