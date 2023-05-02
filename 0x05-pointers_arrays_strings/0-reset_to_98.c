228b8621b0d146d0cb8776ef43f8eacd9167ff0f
ialag <ialaghita94@gmail.com>
2023-05-02 10:26:54 -0700

    sd

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * reset_to_98 t Takes a pomter to an int
- * the valueit points to 98
+ * reset_to_98 - Takes a pomter to an int
+ * the value it points to 98
  * @n: an integer
  * Return: nothing
 */
