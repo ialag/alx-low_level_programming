f14151802f1c6470e974cb6120351c7222dfd34f
ialag <ialaghita94@gmail.com>
2023-06-12 11:41:02 -0700

    git commit -m ''l
     Please enter the commit message for your changes. Lines starting

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,7 +6,7 @@
  * @filename: text file being read
  * @letters: number of letters to be read
  * Return: w- actual number of bytes read and printed
- *        0 when function fails or filename is NULL.
+ * 0 when function fails or filename is NULL.
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
@@ -14,7 +14,6 @@ char *buf;
 ssize_t fd;
 ssize_t w;
 ssize_t t;
-
 fd = open(filename, O_RDONLY);
 if (fd == -1)
 		return (0);
